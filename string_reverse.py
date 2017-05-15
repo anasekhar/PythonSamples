20fac3ad93a4763422e31a8631a10281cdca310b
sekhar <sekharreddy3@gmail.com>
2017-05-15 13:01:10 +0530

    first program commit

---
+++ string_reverse.py
@@ -0,0 +1,10 @@
+
+def string_rev(string):
+   return string[::-1]
+
+
+
+my_string = raw_input(" Enter string to be reversed:")
+rev_str = string_rev(my_string)
+
+print "Reversed string: %s",rev_str
