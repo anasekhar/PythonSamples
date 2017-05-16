d60acfa47a9604dad77eb09dc62a87e65081d344
sekhar <sekharreddy3@gmail.com>
2017-05-17 02:16:11 +0530

    oswalk pgm

---
+++ os_walk_ex.py
@@ -0,0 +1,32 @@
+"""
+ program to print os.walk ( using my function)
+"""
+import argparse
+import os
+import os.path
+
+
+def my_os_walk(path):
+    if not os.path.exists(path):
+	print " Given {0} path is not vaild".format(path) 
+    elif os.path.isdir(path):
+	dir_list = os.listdir(path)
+        for dir_name in dir_list:
+		if os.path.isdir(os.path.join(path,dir_name)):
+		    print "\n%s\n" %(dir_name)
+		    my_os_walk(os.path.join(path,dir_name))
+		else:
+		    print "\t%s\n"%(dir_name)
+	
+	
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Please pase path to the program")
+    parser.add_argument('-p','--path',help='provide dir/file path',required=True)
+    args = parser.parse_args()
+    print args
+    if args.path == 'None':
+	print " Please provide directory/file as a argument"
+    else:
+    	my_os_walk(args.path)
+
